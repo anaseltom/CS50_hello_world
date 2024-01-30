42abd5efbbdfb4c2bd0c6d55adda987414bce323
anas <anas-el-tom@live.com>
2024-01-30 21:06:59 +0400

    with locale time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <h1>Hello World!</h1>;
+  const currentDate = new Date();
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>The time now is {currentDate?.toLocaleTimeString()} </h2>
+    </div>
+  );
 }
 
 export default App;
